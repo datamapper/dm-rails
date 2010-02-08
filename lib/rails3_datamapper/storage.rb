af955097281108758a9a3140e3c96d77ab4a45f7
Dan Kubb <dan.kubb@gmail.com>
2010-02-08 21:51:19 +0100

    Refactored Storage to allow easier adding of new adapters

--- lib/rails3_datamapper/storage.rb
+++ lib/rails3_datamapper/storage.rb
@@ -2,75 +2,129 @@ module Rails
   module DataMapper
 
     def self.storage
-      @storage ||= Storage.new
+      Storage
     end
 
     class Storage
+      attr_reader :name, :config
 
-      def create_all
+      def self.create_all
         with_local_repositories { |config| create_environment(config) }
       end
 
-      def drop_all
+      def self.drop_all
         with_local_repositories { |config| drop_environment(config) }
       end
 
-      def create_environment(config)
-        config.each { |repo_name, repo_config| create(repo_name, repo_config) }
+      def self.create_environment(config)
+        config.each { |repo_name, repo_config| new(repo_name, repo_config).create }
       end
 
-      def drop_environment(config)
-        config.each { |repo_name, repo_config| drop(repo_name, repo_config) }
+      def self.drop_environment(config)
+        config.each { |repo_name, repo_config| new(repo_name, repo_config).drop }
       end
 
-      def create(repository, config)
-        puts "config = #{config.inspect}"
-        database = config['database'] || config['path']
-        case config['adapter']
-        when 'postgres'
-          `createdb -U #{config['username']} #{database}`
-        when 'mysql'
-          user, password = config['username'], config['password']
-          `mysql --user=#{user} #{password ? "--password=#{password}" : ''} -e "create database #{database}"`
-        when 'sqlite3'
-          ::DataMapper.setup(repository.to_sym, config)
+      def self.new(name, config)
+        klass = lookup_class(config['adapter'])
+        if klass.equal?(self)
+          super(name, config)
         else
-          raise "Adapter #{config['adapter']} not supported for creating databases yet."
+          klass.new(name, config)
         end
-        puts "Created database '#{database}'"
       end
 
-      def drop(repository, config)
-        database = config['database'] || config['path']
-        case config['adapter']
-        when 'postgres'
-          `dropdb -U #{config['username']} #{database}`
-        when 'mysql'
-          user, password = config['username'], config['password']
-          `mysql -u #{user} #{password ? "-p #{password}" : ''} -e "drop database #{database}"`
-        when 'sqlite3'
-          require 'pathname'
-          path = Pathname.new(config['database'])
-          file = path.absolute? ? path.to_s : File.join(Rails.root, path)
-          FileUtils.rm(file)
-        else
-          raise "Adapter #{config['adapter']} not supported for dropping databases yet.\ntry db:automigrate"
-        end
-        puts "Dropped database '#{database}'"
-      end
+      class << self
+      private
 
-
-      def with_local_repositories
-        Rails::DataMapper.configuration.repositories.each_value do |config|
-          if %w( 127.0.0.1 localhost ).include?(config['host']) || config['host'].blank?
-            yield(config)
-          else
-            puts "This task only modifies local databases. #{config['database']} is on a remote host."
+        def with_local_repositories
+          Rails::DataMapper.configuration.repositories.each_value do |config|
+            if config['host'].blank? || %w[ 127.0.0.1 localhost ].include?(config['host'])
+              yield(config)
+            else
+              puts "This task only modifies local databases. #{config['database']} is on a remote host."
+            end
           end
+        end
+
+        def lookup_class(adapter)
+          klass_name = adapter.classify.to_sym
+
+          unless const_defined?(klass_name)
+            raise "Adapter #{adapter} not supported"
+          end
+
+          const_get(klass_name)
+        end
+
+      end
+
+      def initialize(name, config)
+        @name, @config = name.to_sym, config
+      end
+
+      def create
+        _create
+        puts "Created database '#{database}' for #{name}"
+      end
+
+      def drop
+        _drop
+        puts "Dropped database '#{database}' for #{name}"
+      end
+
+      def database
+        @database ||= config['database'] || config['path']
+      end
+
+      def username
+        @username ||= config['username']
+      end
+
+      def password
+        @password ||= config['password']
+      end
+
+    private
+
+      class Sqlite < Storage
+        def _create
+          ::DataMapper.setup(name, config)
+        end
+
+        def _drop
+          path = Pathname(database)
+          path = Rails.root.join(path) unless path.absolute?
+          path.unlink
+        end
+      end
+
+      class Mysql < Storage
+        def _create
+          execute("CREATE DATABASE #{database}")
+        end
+
+        def _drop
+          execute("DROP DATABASE #{database}")
+        end
+
+      private
+
+        def execute(command)
+          `mysql --user=#{username} #{password.blank? ? '' : "--password=#{password}"} -e "#{command}"`
+        end
+
+      end
+
+      class Postgres < Storage
+        def _create
+          `createdb -U #{username} #{database}`
+        end
+
+        def _drop
+          `dropdb -U #{username} #{database}`
         end
       end
 
     end
-
   end
 end
