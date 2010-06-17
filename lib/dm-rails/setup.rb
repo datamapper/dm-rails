3323b99f5b5909b16ad750661496161fc77c377e
snusnu <gamsnjaga@gmail.com>
2010-06-17 06:27:07 +0200

    Don't assign naming conventions if none were configured

--- lib/dm-rails/setup.rb
+++ lib/dm-rails/setup.rb
@@ -18,7 +18,9 @@ module Rails
     def self.setup_with_instrumentation(name, options)
       ::DataMapper.logger.info "[datamapper] Setting up #{name.inspect} repository: '#{options['database']}' on #{options['adapter']}"
       adapter = ::DataMapper.setup(name, options)
-      adapter.resource_naming_convention = configuration.resource_naming_convention[name]
+      if configuration.resource_naming_convention[name]
+        adapter.resource_naming_convention = configuration.resource_naming_convention[name]
+      end
       adapter.extend ::DataMapper::Adapters::Benchmarking
       setup_log_listener(options['adapter'])
     end
