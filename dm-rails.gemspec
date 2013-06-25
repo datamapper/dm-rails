ca7d3c16bb6f827675601f1f042e52c956f881f1
Dan Kubb <dan.kubb@gmail.com>
2013-06-25 13:27:16 -0700

    Update dm-rails to work with rails 4

--- dm-rails.gemspec
+++ dm-rails.gemspec
@@ -9,7 +9,7 @@ Gem::Specification.new do |s|
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Martin Gamsjaeger (snusnu)", "Dan Kubb"]
-  s.date = "2012-02-15"
+  s.date = "2013-06-25"
   s.description = "Integrate DataMapper with Rails 3"
   s.email = "gamsnjaga@gmail.com"
   s.extra_rdoc_files = [
@@ -18,6 +18,7 @@ Gem::Specification.new do |s|
   ]
   s.files = [
     ".document",
+    ".travis.yml",
     "Gemfile",
     "LICENSE",
     "README.rdoc",
@@ -58,25 +59,25 @@ Gem::Specification.new do |s|
   s.homepage = "http://github.com/datamapper/dm-rails"
   s.require_paths = ["lib"]
   s.rubyforge_project = "datamapper"
-  s.rubygems_version = "1.8.11"
+  s.rubygems_version = "2.0.3"
   s.summary = "Use DataMapper with Rails 3"
 
   if s.respond_to? :specification_version then
-    s.specification_version = 3
+    s.specification_version = 4
 
     if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
       s.add_runtime_dependency(%q<dm-core>, ["~> 1.2.0"])
       s.add_runtime_dependency(%q<dm-active_model>, ["~> 1.2.0"])
-      s.add_runtime_dependency(%q<actionpack>, ["~> 3.0"])
-      s.add_runtime_dependency(%q<railties>, ["~> 3.0"])
+      s.add_runtime_dependency(%q<actionpack>, ["< 5.0", ">= 3.0"])
+      s.add_runtime_dependency(%q<railties>, ["< 5.0", ">= 3.0"])
       s.add_development_dependency(%q<jeweler>, ["~> 1.6.4"])
       s.add_development_dependency(%q<rake>, ["~> 0.9.2"])
       s.add_development_dependency(%q<rspec>, ["~> 1.3.2"])
     else
       s.add_dependency(%q<dm-core>, ["~> 1.2.0"])
       s.add_dependency(%q<dm-active_model>, ["~> 1.2.0"])
-      s.add_dependency(%q<actionpack>, ["~> 3.0"])
-      s.add_dependency(%q<railties>, ["~> 3.0"])
+      s.add_dependency(%q<actionpack>, ["< 5.0", ">= 3.0"])
+      s.add_dependency(%q<railties>, ["< 5.0", ">= 3.0"])
       s.add_dependency(%q<jeweler>, ["~> 1.6.4"])
       s.add_dependency(%q<rake>, ["~> 0.9.2"])
       s.add_dependency(%q<rspec>, ["~> 1.3.2"])
@@ -84,8 +85,8 @@ Gem::Specification.new do |s|
   else
     s.add_dependency(%q<dm-core>, ["~> 1.2.0"])
     s.add_dependency(%q<dm-active_model>, ["~> 1.2.0"])
-    s.add_dependency(%q<actionpack>, ["~> 3.0"])
-    s.add_dependency(%q<railties>, ["~> 3.0"])
+    s.add_dependency(%q<actionpack>, ["< 5.0", ">= 3.0"])
+    s.add_dependency(%q<railties>, ["< 5.0", ">= 3.0"])
     s.add_dependency(%q<jeweler>, ["~> 1.6.4"])
     s.add_dependency(%q<rake>, ["~> 0.9.2"])
     s.add_dependency(%q<rspec>, ["~> 1.3.2"])
--- lib/dm-rails/mass_assignment_security.rb
+++ lib/dm-rails/mass_assignment_security.rb
@@ -3,87 +3,90 @@ require 'active_support/core_ext/class/attribute'
 require 'active_support/concern'
 require 'active_model'
 
-module ActiveModel
-  module MassAssignmentSecurity
-    # Provides a patched version of the Sanitizer used in Rails to handle property
-    # and relationship objects as keys. There is no way to inject a custom sanitizer
-    # without reimplementing the permission sets.
-    Sanitizer.send(Sanitizer.is_a?(Module) ? :module_eval : :class_eval) do
-      # Returns all attributes not denied by the authorizer.
-      #
-      # @param [Hash{Symbol,String,::DataMapper::Property,::DataMapper::Relationship=>Object}] attributes
-      #   Names and values of attributes to sanitize.
-      # @return [Hash]
-      #   Sanitized hash of attributes.
-      def sanitize(attributes, authorizer = nil)
-        sanitized_attributes = attributes.reject do |key, value|
-          key_name = key.name rescue key
-          authorizer ? authorizer.deny?(key_name) : deny?(key_name)
+if defined?(::ActiveModel::MassAssignmentSecurity)
+  module ActiveModel
+    module MassAssignmentSecurity
+      # Provides a patched version of the Sanitizer used in Rails to handle property
+      # and relationship objects as keys. There is no way to inject a custom sanitizer
+      # without reimplementing the permission sets.
+      Sanitizer.send(Sanitizer.is_a?(Module) ? :module_eval : :class_eval) do
+        # Returns all attributes not denied by the authorizer.
+        #
+        # @param [Hash{Symbol,String,::DataMapper::Property,::DataMapper::Relationship=>Object}] attributes
+        #   Names and values of attributes to sanitize.
+        # @return [Hash]
+        #   Sanitized hash of attributes.
+        def sanitize(attributes, authorizer = nil)
+          sanitized_attributes = attributes.reject do |key, value|
+            key_name = key.name rescue key
+            authorizer ? authorizer.deny?(key_name) : deny?(key_name)
+          end
+          debug_protected_attribute_removal(attributes, sanitized_attributes)
+          sanitized_attributes
         end
-        debug_protected_attribute_removal(attributes, sanitized_attributes)
-        sanitized_attributes
       end
     end
   end
-end
 
-module DataMapper
-  # Include this module into a DataMapper model to enable ActiveModel's mass
-  # assignment security.
-  #
-  # To use second parameter of {#attributes=} make sure to include this module
-  # last.
-  module MassAssignmentSecurity
-    extend ::ActiveSupport::Concern
-    include ::ActiveModel::MassAssignmentSecurity
+  module DataMapper
+    # Include this module into a DataMapper model to enable ActiveModel's mass
+    # assignment security.
+    #
+    # To use second parameter of {#attributes=} make sure to include this module
+    # last.
+    module MassAssignmentSecurity
+      extend ::ActiveSupport::Concern
 
-    module ClassMethods
-      extend ::ActiveModel::MassAssignmentSecurity::ClassMethods
+      include ::ActiveModel::MassAssignmentSecurity
 
-      def logger
-        @logger ||= ::DataMapper.logger
+      module ClassMethods
+        extend ::ActiveModel::MassAssignmentSecurity::ClassMethods
+
+        def logger
+          @logger ||= ::DataMapper.logger
+        end
       end
-    end
 
-    # Sanitizes the specified +attributes+ according to the defined mass-assignment
-    # security rules and calls +super+ with the result.
-    #
-    # Use either +attr_accessible+ to specify which attributes are allowed to be
-    # assigned via {#attributes=}, or +attr_protected+ to specify which attributes
-    # are *not* allowed to be assigned via {#attributes=}.
-    #
-    # +attr_accessible+ and +attr_protected+ are mutually exclusive.
-    #
-    # @param [Hash{Symbol,String,::DataMapper::Property,::DataMapper::Relationship=>Object}] attributes
-    #   Names and values of attributes to sanitize.
-    # @param [Boolean] guard_protected_attributes
-    #   Determines whether mass-security rules are applied (when +true+) or not.
-    # @return [Hash]
-    #   Sanitized hash of attributes.
-    # @api public
-    #
-    # @example [Usage]
-    #   class User
-    #     include DataMapper::Resource
-    #     include DataMapper::MassAssignmentSecurity
-    #
-    #     property :name, String
-    #     property :is_admin, Boolean
-    #
-    #     # Only allow name to be set via #attributes=
-    #     attr_accessible :name
-    #   end
-    #
-    #   user = User.new
-    #   user.attributes = { :username => 'Phusion', :is_admin => true }
-    #   user.username  # => "Phusion"
-    #   user.is_admin  # => false
-    #
-    #   user.send(:attributes=, { :username => 'Phusion', :is_admin => true }, false)
-    #   user.is_admin  # => true
-    def attributes=(attributes, guard_protected_attributes = true)
-      attributes = sanitize_for_mass_assignment(attributes) if guard_protected_attributes
-      super(attributes)
+      # Sanitizes the specified +attributes+ according to the defined mass-assignment
+      # security rules and calls +super+ with the result.
+      #
+      # Use either +attr_accessible+ to specify which attributes are allowed to be
+      # assigned via {#attributes=}, or +attr_protected+ to specify which attributes
+      # are *not* allowed to be assigned via {#attributes=}.
+      #
+      # +attr_accessible+ and +attr_protected+ are mutually exclusive.
+      #
+      # @param [Hash{Symbol,String,::DataMapper::Property,::DataMapper::Relationship=>Object}] attributes
+      #   Names and values of attributes to sanitize.
+      # @param [Boolean] guard_protected_attributes
+      #   Determines whether mass-security rules are applied (when +true+) or not.
+      # @return [Hash]
+      #   Sanitized hash of attributes.
+      # @api public
+      #
+      # @example [Usage]
+      #   class User
+      #     include DataMapper::Resource
+      #     include DataMapper::MassAssignmentSecurity
+      #
+      #     property :name, String
+      #     property :is_admin, Boolean
+      #
+      #     # Only allow name to be set via #attributes=
+      #     attr_accessible :name
+      #   end
+      #
+      #   user = User.new
+      #   user.attributes = { :username => 'Phusion', :is_admin => true }
+      #   user.username  # => "Phusion"
+      #   user.is_admin  # => false
+      #
+      #   user.send(:attributes=, { :username => 'Phusion', :is_admin => true }, false)
+      #   user.is_admin  # => true
+      def attributes=(attributes, guard_protected_attributes = true)
+        attributes = sanitize_for_mass_assignment(attributes) if guard_protected_attributes
+        super(attributes)
+      end
     end
   end
 end
